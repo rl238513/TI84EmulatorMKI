830f7413198b0b0afeef835e3e35459dbece28ce
Eduardo Mendoza <rl238513@stu.rlas-116.org>
2019-09-13 12:40:31 -0500

    set up pt3

--- TI84EmulatorMKI/ViewController.swift
+++ TI84EmulatorMKI/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberScreen: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    @IBAction func helpButton(_ sender: Any) {
+    }
+    @IBAction func graphButton(_ sender: Any) {
+    }
+    @IBAction func settingsButton(_ sender: Any) {
+    }
+    
 
 }
 
