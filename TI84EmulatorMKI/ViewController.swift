73fe09624881e79073aa3a1a7dd93a4537564cef
Eduardo Mendoza <rl238513@stu.rlas-116.org>
2019-09-16 12:45:32 -0500

    finished buttons idk how to code them tho

--- TI84EmulatorMKI/ViewController.swift
+++ TI84EmulatorMKI/ViewController.swift
@@ -12,18 +12,57 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var numberScreen: UILabel!
     
+    var equation: String = ""
+    var array: [Any] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func zeroButton(_ sender: Any) {
+        array.append(0)
     }
     @IBAction func oneButton(_ sender: Any) {
+        array.append(1)
     }
     @IBAction func twoButton(_ sender: Any) {
+        array.append(2)
     }
     @IBAction func threeButton(_ sender: Any) {
+    }
+    @IBAction func fourButton(_ sender: Any) {
+    }
+    @IBAction func fiveButton(_ sender: Any) {
+    }
+    @IBAction func sixButton(_ sender: Any) {
+    }
+    @IBAction func sevenButton(_ sender: Any) {
+    }
+    @IBAction func eightButton(_ sender: Any) {
+    }
+    @IBAction func nineButton(_ sender: Any) {
+    }
+    @IBAction func decimalButton(_ sender: Any) {
+    }
+    @IBAction func negativeButton(_ sender: Any) {
+    }
+    @IBAction func enterButton(_ sender: Any) {
+    }
+    @IBAction func onOffButton(_ sender: Any) {
+    }
+    @IBAction func additionButton(_ sender: Any) {
+    }
+    @IBAction func subtractionButton(_ sender: Any) {
+    }
+    @IBAction func multiplicationButton(_ sender: Any) {
+    }
+    @IBAction func divisionButton(_ sender: Any) {
+    }
+    @IBAction func openParenthesisButton(_ sender: Any) {
+    }
+    @IBAction func closedParenthesisButton(_ sender: Any) {
     }
     
     
